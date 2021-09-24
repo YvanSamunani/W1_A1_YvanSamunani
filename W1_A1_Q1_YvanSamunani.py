a94cda65fcfd6e93c1986e16a9806976d926c4ef
Yvan Samunani <yvansamu@gmail.com>
2021-09-24 23:28:43 +0200

    W1_A1_YvanSumunani

---
+++ W1_A1_Q1_YvanSamunani.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Store input numbers
+# importing the required module
+
+firstName = "Yvan"
+LastName = "Samunani"
+print(firstName[0], '   ', firstName[0], ' ', LastName[0]*5)
+print(' ', firstName[0], firstName[0], '   ', LastName[0])
+print('  ', firstName[0], '    ', LastName[0])
+print('  ', firstName[0], '    ', LastName[0]*5)
+print('  ', firstName[0], '    ', '   ', LastName[0])
+print('  ', firstName[0], '    ', '   ', LastName[0])
+print('  ', firstName[0], '    ', LastName[0]*5)
+
+# importing libraries
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ W1_A1_Q3_YvanSamunani.py
@@ -0,0 +1,107 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+answer_1= input("Q1) When was ALU founded? : ")
+if answer_1=="2015":
+    print("Correct")
+    Score=1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score=0
+    wrong_answers=1
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_2 = input("Q2) Who is the CEO of ALU? : ")
+if answer_2=="Fred Swaniker" or answer_2== "Swaniker Fred":
+    print("Correct")
+    Score +=1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score+=0
+    print("Your Score is: ", Score, "/10")
+
+answer_3 = input("Q3) Where are ALU campuses : ")
+if answer_3 =="Rwanda and Mauritius" or answer_3 == "Mauritius and Rwanda":
+    print("Correct")
+    Score +=1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score+=0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_4 = input("Q4) How many campuses does ALU have : ")
+if answer_4 == "2" :
+    print("Correct")
+    Score +=1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score+=0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_5 = input("Q5) What is the name of ALU Rwanda’s Dean : ")
+if answer_5 == "Dr. Gaidi Faraj" or answer_5 == "Gaidi Faraj" or answer_5 == "Faraj Gaidi":
+    print("Correct")
+    Score +=1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score+=0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_6 = input("Q6) Who is in charge of Student Life : ")
+if answer_6 == "Sila Ogidi" or answer_6 =="Ogidi Sila":
+    print("Correct")
+    Score += 1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score += 0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_7 = input("Q7) What is the name of our Lab? : ")
+if answer_7 == "Fablab":
+    print("Correct")
+    Score += 1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score += 0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_8 = input("Q8) How many students do we have in Year 2 CS? : ")
+if answer_8 == "137":
+    print("Correct")
+    Score += 1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score += 0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_9 = input("Q9) How many degrees does ALU offer? : ")
+if answer_9 == "8":
+    print("Correct")
+    Score += 1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score += 0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+answer_10 = input("Q10) Where are the headquarters of ALU : ")
+if answer_10 == "Mauritius":
+    print("Correct")
+    Score += 1
+    print("Your Score is: ", Score, "/10")
+else:
+    print("Wrong")
+    Score += 0
+    print("Your Score is: ", Score, "/10","\n" "you are hanging the man")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
